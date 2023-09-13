5d278a294f1e1472a945b4cd0343bb39cfc53f13
Alia Lescoulie <alia.lescoulie@gmail.com>
2023-09-12 22:30:53 -0700

    update build.py

--- build.py
+++ build.py
@@ -4,13 +4,18 @@ import os
 
 from pathlib import Path
 
+import shutil
+
 import glob
 
 from jinja2 import Environment, Template, FileSystemLoader, select_autoescape
 
 
 # Pre-defined site names
-BUILD_DIR: Final[Path] = Path("Site")
+BUILD_DIR: Final[Path] = Path("site")
+SRC_DIR: Final[Path] = Path("src")
+STATIC_DIR: Final[Path] = Path("static")
+
 
 
 def make_build_dir() -> None:
@@ -19,10 +24,9 @@ def make_build_dir() -> None:
     elif BUILD_DIR.exists() and not BUILD_DIR.is_dir():
         os.remove(BUILD_DIR)
     os.mkdir(BUILD_DIR) 
-    return
 
 
-def build_pages() -> int:
+def build_pages() -> None:
     make_build_dir() # Ensures that build_dir exists
     TemplatesBase: Environment = Environment(
         loader=FileSystemLoader("templates"),
@@ -44,37 +48,30 @@ def build_pages() -> int:
         page_temp: Template = Pages.get_template(page_path.stem + page_path.suffix)
 
         page_title: str = "Alia Lescoulie" if page_path.stem == "index.html" else \
-        f"{str(page_path.stem)[:-5].capitalize()} - Alia Lesoculie"
+        f"{str(page_path.stem)[:-5].capitalize()} - Alia Lescoulie"
 
         page_header: str = header.render(title=page_title)
 
         page_text: str = page_temp.render(header=page_header, navbar=navbar.render())
 
         with open(BUILD_DIR.joinpath(page_path.stem), 'w', encoding='utf-8') as file:
-            file.write(page_text)
-        
+            file.write(page_text) 
     
+
+def copy_static():
+    make_build_dir()
+    shutil.copytree(STATIC_DIR, Path(BUILD_DIR, "static"))
+
 
 def build_blog() -> int:
     pass
 
 
-def build_full() -> int:
-    
-
-    build_pages()
-
-    build_blog()
-
-
-
-
-    return 0
-
-
 def clean():
-    pass
+    shutil.rmtree(BUILD_DIR)
 
 
 if __name__ == "__main__":
+    clean()
     build_pages()
+    copy_static()

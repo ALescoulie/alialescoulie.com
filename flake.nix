b7c66441a8ce09ac3c96fa9b381ec89abc0e46d0
Alia Lescoulie <alia.lescoulie@gmail.com>
2023-10-09 15:00:18 -0700

    add nix flake and update poetry files

--- flake.nix
+++ flake.nix
@@ -3,8 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        customOverrides = final: prev: {
+          pandoc = prev.pandoc.overrideAttrs (oldAttrs: {
+            buildInputs = [ prev.setuptools ];
+            runtimeInputs = [ prev.setuptools ];
+          });
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides =
+            [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+        };
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        packageName = "alialescoulie.com";
+      in {
+        packages.${packageName} = app;
+
+        packages.default = self.packages.${system}.${packageName};
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
 }

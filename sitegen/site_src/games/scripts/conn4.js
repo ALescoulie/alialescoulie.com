47d292a4447c58824919f479b3ab77ca12e3dd4b
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-01-19 15:10:20 -0800

    make conn4 have option for two players

--- sitegen/site_src/games/scripts/conn4.js
+++ sitegen/site_src/games/scripts/conn4.js
@@ -1,12 +1,41 @@
-function getDifficulty() {
-    var inputs = document.getElementsByTagName("input");
+function getPlayers() {
+    let inputs = document.getElementById("input")
     for (i = 0; i < inputs.length; i++) {
-        var input = inputs[i];
+        let input = inputs[i];
+
+        if (input.type === "radio" && input.name === "players") {
+            return input.value;
+        }
+    }
+}
+
+
+function getDifficulty() {
+    let inputs = document.getElementsByTagName("input");
+    for (i = 0; i < inputs.length; i++) {
+        let input = inputs[i];
 
         if (input.type === "radio" && input.checked) {
             return input.value;
         }
     }
+}
+
+
+class GameState {
+    constructor(n_players, board, table, game_out) {
+        this.m_players = n_players;
+        this.board = board;
+        this.table = table;
+        this.game_out = game_out;
+    }
+
+    get board() {
+        return this.board();
+    }
+
+    
+
 }
 
 
@@ -53,10 +82,8 @@ function updateTable(table, board) {
         let row = table.rows[i];
         for (let j = 0; j < board[i].length; j++) {
             row.cells[j].firstChild.replaceWith(placeBoardImage(board, i, j));
-
         }
     }
-
 }
 
 
@@ -77,30 +104,125 @@ function onClickColumn(col, board, table) {
     row = lowestEmptyRow(board, col);
 
     if (row != null) {
-        board[row][col] = 1;
+        board[row][col] = player;
         placeBoardImage(board, row, col);
         updateTable(table, board);
+        game_status = gameWon(board);
+        if (game_status != null) {
+            console.log(game_status);
+        }
+
+        if (game_status === RED) {
+            game_out.innerHTML = "You won!";
+        } else if (game_status === BLACK) {
+            game.innerHTML = "You Lose!";
+        }
     }
+}
+
+
+const RED = Symbol("red")
+const BLACK = Symbol("black")
+
+
+function gameWon(board) {
+    // check for horizontal wins
+
+    for (let i = 0; i < board.length; i++) {
+        let row = board[i];
+        let count = 0;
+        let cur_color = null;
+        let j = 0;
+
+        while (count < 4 && j < row.length) {
+            if (row[j] != 0 && cur_color === null) {
+                cur_color = row[j]
+                count++;
+            } else if (cur_color != null && row[j] === cur_color) {
+                count++;
+            } else if (cur_color != null && row[j] != cur_color && row[j] != 0) {
+                cur_color = row[j];
+                count = 1;
+            } else if (cur_color != null && row[j] != cur_color && row[j] === 0) {
+                cur_color = null;
+                count = 0;
+            }
+            j++;
+        }
+
+        if (count === 4 && cur_color === 1) {
+            return RED;
+        } else if (count === 4 && cur_color === 2) {
+            return BLACK;
+        }
+    }
+
+    // check for verticle wins
+
+    for (j = 0; j < board[0].length; j++) {
+        count = 0;
+        cur_color = null;
+        i = 0;
+
+        while(count < 4 && i < board.length) {
+            if (board[i][j] != 0 && cur_color === null) {
+                cur_color = board[i][j];
+                count++;
+            } else if (board[i][j] != 0 && board[i][j] != cur_color && cur_color != null) {
+                cur_color = board[i][j];
+                count = 1;
+            } else if (board[i][j] === cur_color && cur_color != null) {
+                count++;
+            } else if (cur_color != null && board[i][j] === 0) {
+                cur_color = null;
+                count = 0;
+            }
+            i++;
+        }
+
+        if (count === 4 && cur_color === 1) {
+            return RED;
+        } else if (count === 4 && cur_color === 2) {
+            return BLACK;
+        }
+        
+    }
+    return null;
+
+    // check for diagional
 }
 
 
 function startGame() {
 
-    const game_out = document.getElementById("game_out");
-    const difficulty = getDifficulty();
-
-    game_out.innerHTML = difficulty.toString();
-    
-    console.log("difficulty" + difficulty.toString());
+    var game_out = document.getElementById("game_out");
+    var game_status = null;
 
     const board = generateBoard(6, 7);
-
     const table = document.getElementById("conn4_board");
-    
+
     buildTable(table, board);
 
-    game_out.innerHTML = "Make your move.";
+    const n_players = getPlayers();
+    var player = 1;
 
-    
+    if (n_players === 1) {
+        const difficulty = getDifficulty();
+
+        game_out.innerHTML = difficulty.toString();
+        
+        console.log("difficulty" + difficulty.toString());
+    } else if (n_players === 2) {
+        multiPlayer(board, table);   
+    }
+
+
+
 }
 
+
+function multiPlayer(board, table) {
+    game_out.innerHTML = "Player1: Make your move.";
+
+
+}
